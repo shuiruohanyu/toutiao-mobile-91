8389ac4e31c93f0165d0c8fb47483b1ce3eb83f9
gaoly <shuiruohanyu@foxmail.com>
2020-01-10 22:00:59 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css' // 引入样式
+Vue.use(Vant) // 全局注册
 Vue.config.productionTip = false
 
 new Vue({
